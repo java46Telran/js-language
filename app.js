fcadd5609458044c20e50de730f2fca582caf13b
Yuragranovsky <yuragranovsky@gmail.com>
2022-01-17 21:08:13 +0200

    cw #12 with definition of hw #12 and work with arrays

--- app.js
+++ app.js
@@ -1,74 +1,71 @@
-/*************************Strings */
-const str1 = "abc";
-const str2 = "abc";
-// console.log(`str1 == str2 is ${str1 == str2}`)
-// console.log(`"abc" < "ab" is ${str1 < "ab"}`)
-// console.log(`"123" > 23 is ${"123" > 23}`);
-// console.log(`"123" > "23" is ${"123" > "23"}`);
-/****************************************** */
-//if  logical expression contains both string and number, JS will convert string to number
-//if string doesn't contain a number a result of conversion will be NaN
-//In any logical expression if there is NaN, then the result will be false
-// console.log(`"abc" > 23 is ${"abc" > 23}`);
-// console.log(`"abc" < 23 is ${"abc" < 23}`);
-// console.log(`"abc" != 23 is ${!("abc" == 23)}`)
-/********************************************* */
-function stringProcessing(str) {
-        const strP = "" + str;
-       console.log(`"${str}"[4] is ${str[4]}`);
-       console.log(`length of "${str}" is ${strP.length}`);
-       console.log(`"${str}" includes "abc" is ${strP.includes("abc")}`);
-       console.log(`substring of "${str}" beginning from index 2 to index 5 is ${strP.substring(2, 5)}`);
-       console.log(`index of substring "ll" in the "${str}" is ${strP.indexOf("ll")}; last index of substring "ll" in the "${str}" is  ${strP.lastIndexOf("ll")}`)
+/**********************Arrays */
+const ar = [2, 100, 9, 80];
+// ar.sort(function (a, b){
+//         return a - b;
+// });
+// ar.sort((a,b) => a - b)
+// console.log(`ar is ${ar.toString()}`);
 
+//adding element/s at end of an array
+ar[ar.length] = 200;
+console.log(ar);
+ar.push(300, 25);
+console.log(ar);
+const ar1 = [-8, 30, -57];
+// ar.push(ar1); there will be added array but not separated numbers
+// if you want to add separated numbers you should apply a spread operator ...
+ar.push(...ar1);
+console.log(ar); 
+//for adding elements at beginning of an array there is method "unshift"
+//everything has been said about "push" will work for "unshift" only unlike "push" unshift method
+//adds elements at beginning
+const ar2 = [27, 35];
+ar.unshift(...ar2);
+console.log(ar);
+//abc123drtyugr
+//method splice for inserting or replacing any elements in any place
+//first argument - index for inserting or replacing
+//second argument - number of the deleted elements (if 0 than splice will be applied only for inserting)
+//third argiment designates sequence of being inserted element like push/unshift
+ar.splice(2,0,1,2);
+console.log(ar);
+//removing (deleting)
+let el = ar.pop(); //removes the last element and returns it
+//el = -57; -57 will be removed from the array ar
+el = ar.shift(); //removes the first element and returns it
+//el = 27; 27 will be removed from the array ar
+console.log(el, ar);
+ar.splice(3, 5); //remove 5 elements beginning from the one at index 3
+console.log(ar);
+//includes, indexOf
+//two dimensional arrays
+const matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [3, 4, 5]];
+const matrix2 = [[10, 20], [50, -5], [34, 28]]
+function displayMatrix(matrix) {
+     for (let i = 0; i < matrix.length; i++) {
+             let row = '';
+             for (let j = 0; j < matrix[i].length; j++) {
+                     row = row + matrix[i][j] + '   '
+             }
+             console.log(row);
+     }  
 }
-//stringProcessing("abcd*lmn*ll(oo&&ttll");
-/********************HW 11 definition */
-// function encode(num, base) {
-//         //base from 2 to 10
-//         let res="";
-//         do {
-//                 const digit = Math.trunc(num % base);
-//                 const symb = getSymbol(digit);
-//                 res = symb + res;
-//                 num = Math.trunc(num / base);
-
-//         } while(num >= 1);
-//         return res;
-// }
-// function getSymbol(digit) {
-//       //base from 2 to 10 
-//       console.log(digit);
-//       return "" + digit; //it will work only for base <= 10 
-// }
-//console.log(encode(10, 2))
+//displayMatrix(matrix1);
+displayMatrix(matrix2);
+/******************HW #12 definition */
+const arHw = [13, 28, 4, 15, 25, -10, 40, 17,27];
+//expected [-10, 4, 28, 40, 27, 25, 17, 15, 13]
+//task: write a comparator (a comparator returns < 0 if first less than second, >0 
+// if first greater than second, ==0 if first equals second)
+/************************************************************** */
+/**************************************HW 12 definition */
 // write function 
-function encode (num, codingString) {
-        //codingString - any string with no repeated symbols
-        // you should validate codingString doesn't contain repeated symbols
-        //base = length of codingString
-        // algorithm the same as specified above
-        // getSymbol(digit, codingString) using operator []
-        let flRepeated = false;
-        for (let i = 0; i < codingString.length; i++) {
-                if(codingString.lastIndexOf(codingString[i]) != i) {
-                        console.log(`coding string has repeated symbol ${codingString[i]}`)
-                        flRepeated = true;
-                }
-        }
-        if (!flRepeated) {
-            let res = "" ;
-            const base = codingString.length;
-            do {
-                    const digit = Math.trunc(num % base);
-                    const digitSymb = codingString[digit];
-                    res = digitSymb + res;
-                    num = Math.trunc(num / base);
-
-            } while(num != 0); 
-            return res;  
-        }
+function matrixTransp(matrix) {
+        //TODO
+        //returns matrix with columns that are rows of the source matrix and 
+        //rows that are columns of the source matrix
+        //example: source matrix [[1,2], [3,4], [5,6]]
+        // result matrix [[1, 3, 5], [2, 4, 6]]
 }
-console.log(`encode(5, '.-') = ${encode(5, '.-')}`);
-console.log(`encode(5, '..-') = ${encode(5, '..-')}`);
-console.log(`encode(555, '&*()%$#@!+') = ${encode(555, '&*()%$#@!+')}`);
+
+
