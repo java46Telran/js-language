2a41253e59b072ca13cedd087cbcdd1b4740ebbd
Yuragranovsky <yuragranovsky@gmail.com>
2022-01-20 21:13:21 +0200

    cw #12 with hw definition (two tasks myForEach, myMap)

--- app.js
+++ app.js
@@ -1,95 +1,48 @@
-/**********************Arrays */
-const ar = [2, 100, 9, 80];
-// ar.sort(function (a, b){
-//         return a - b;
-// });
-// ar.sort((a,b) => a - b)
-// console.log(`ar is ${ar.toString()}`);
-
-//adding element/s at end of an array
-ar[ar.length] = 200;
-console.log(ar);
-ar.push(300, 25);
-console.log(ar);
-const ar1 = [-8, 30, -57];
-// ar.push(ar1); there will be added array but not separated numbers
-// if you want to add separated numbers you should apply a spread operator ...
-ar.push(...ar1);
-console.log(ar); 
-//for adding elements at beginning of an array there is method "unshift"
-//everything has been said about "push" will work for "unshift" only unlike "push" unshift method
-//adds elements at beginning
-const ar2 = [27, 35];
-ar.unshift(...ar2);
-console.log(ar);
-//abc123drtyugr
-//method splice for inserting or replacing any elements in any place
-//first argument - index for inserting or replacing
-//second argument - number of the deleted elements (if 0 than splice will be applied only for inserting)
-//third argiment designates sequence of being inserted element like push/unshift
-ar.splice(2,0,1,2);
-console.log(ar);
-//removing (deleting)
-let el = ar.pop(); //removes the last element and returns it
-//el = -57; -57 will be removed from the array ar
-el = ar.shift(); //removes the first element and returns it
-//el = 27; 27 will be removed from the array ar
-console.log(el, ar);
-ar.splice(3, 5); //remove 5 elements beginning from the one at index 3
-console.log(ar);
-//includes, indexOf
-//two dimensional arrays
-const matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [3, 4, 5]];
-const matrix2 = [[10, 20], [50, -5], [34, 28]]
-function displayMatrix(matrix) {
-     for (let i = 0; i < matrix.length; i++) {
-             let row = '';
-             for (let j = 0; j < matrix[i].length; j++) {
-                     row = row + matrix[i][j] + '   '
-             }
-             console.log(row);
-     }  
+function mapper (n, i, a) {
+       return `<li>${i + 1} of ${a.length} - ${n}</li>`    
 }
-//displayMatrix(matrix1);
-displayMatrix(matrix2);
-/******************HW #12 definition */
-//const arHw = [13, 28, 4, 15, 25, -10, 40, 17,27];
-//expected [-10, 4, 28, 40, 27, 25, 17, 15, 13]
-//task: write a comparator (a comparator returns < 0 if first less than second, >0 
-// if first greater than second, ==0 if first equals second)
-/************************************************************** */
-/**************************************HW 12 definition */
-// write function 
-//function matrixTransp(matrix) {
-        //TODO
-        //returns matrix with columns that are rows of the source matrix and 
-        //rows that are columns of the source matrix
-        //example: source matrix [[1,2], [3,4], [5,6]]
-        // result matrix [[1, 3, 5], [2, 4, 6]]
-//}
+function someFunction() {
+        const ar = [-10, 50, -12, 80, 90];
+        const ar3 = ar.map(mapper);
+console.log(ar3);
 
-/************************************HW# 12 Solution task 1*/
-const arHw = [13, 28, 4, 15, 25, -10, 40, 17,27];
-//expected [-10, 4, 28, 40, 27, 25, 17, 15, 13]
-const comparator = (n1, n2) => {
-        let res = n1 % 2 - n2 % 2;
-        if (res == 0) {
-                res = n1 % 2 == 0 ? n1 - n2 : n2 - n1; 
-        }
-        return res;
 }
-arHw.sort(comparator);
-console.log(arHw);
-/******************************************************HW #12 solution task2 */
-function matrixTransp(matrix) {
-        const res = [];
+const ar = [-10, 50, -12, 80, 40];
+ar.push(70);
+// str - "-10#50#-12#80#40#70"
+/*********solution based on substring */
+// let str = '';
+// ar.forEach(function(n) {
+//         str += n + '#';
+// })
+// str = str.substring(0, str.length - 1);
+/*************************solution based on forEach form second number */
+// const ar1 = ar.slice(1);
+// let str =  '' + ar[0];
+// ar1.forEach(n => str += '#' + n);
+// console.log(str);
+/*****************************printing out sequense number of element, element, length of arry */
+// 1 of 5 - -10;    ....
+ar.forEach((n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
 
-        for (let i = 0; i < matrix[0].length; i++) {
-                res.push([]);
-                for (let j = 0; j < matrix.length; j++) {
-                        res[i].push(matrix[j][i]);
-                }
-        }
-        return res;
-}
-displayMatrix(matrixTransp([[1,2], [3,4], [5,6]]));
+// method "map"
+//use case of applying method map : you want to create new array with elements that are received as result of some conversion
+//example you want to get new array with elements that are muliplication on 2 of each source element
+const ar2 = ar.map(n => n * 2);
+console.log(ar2);
+someFunction();
+/*****************HW 13 task1 definition */
+// write function myForEach(array, callback-function);
+//array  - being iterated array
+//callback-function - function that will be called for each element of array
+//callback-function should take three arguments: current elemnt, current index, being iterated array
+//example of standard forEach : array.forEach(n => str += '#' + n);
+//example of myForEach : myForEach(array, n => str += '#' + n);
+
+/********************************************************************* */
+/**********************************************HW 13 definition task2 */
+//write method myMap  for the same functionality as standard method map
+//function myMap(array, callback-function)
+//myMap will apply your method myForEach
+
+
