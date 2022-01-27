425bf433406ae7c44042b4017b5a6281efdb771f
Yuragranovsky <yuragranovsky@gmail.com>
2022-01-27 18:11:46 +0200

    hw #14 solution

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const person1 = { id: 123, name: 'Moshe', address: { city: 'Lod', street: 'Sokolov' } };
-const person2 = { id: 123, name: 'Moshe', address: { city: 'Lod', street: 'Sokolov' } };
-const person3 = person1;
+// const person1 = { id: 123, name: 'Moshe', address: { city: 'Lod', street: 'Sokolov' } };
+// const person2 = { id: 123, name: 'Moshe', address: { city: 'Lod', street: 'Sokolov' } };
+// const person3 = person1;
 
-console.log(`"123" == 123 is ${"123" == 123}`);
-console.log(`"123" === 123 is ${"123" === 123}`);
-console.log(`person1 === person3 is ${person1 === person3}`)
-console.log(`person1 === person2 is ${person1 === person2}`)
-console.log(`JSON.stringify(person1) === JSON.stringify(person2) is ${JSON.stringify(person1) ===
-        JSON.stringify(person2)}`);
-console.log(JSON.stringify(person1));
-console.log(person1.toString());
-console.log(`name of person1 is ${person1.name}`)
-console.log(`person1 lives in city ${person1.address.city}`);
-Object.keys(person1).forEach(k => console.log(k)) //array of the object keys
-Object.values(person1).forEach(v => console.log(v)); //array of the object values
-Object.entries(person1).forEach(e => console.log(e)); //array of arrays - [key, value]
-console.log(Object.entries(person1));
+// console.log(`"123" == 123 is ${"123" == 123}`);
+// console.log(`"123" === 123 is ${"123" === 123}`);
+// console.log(`person1 === person3 is ${person1 === person3}`)
+// console.log(`person1 === person2 is ${person1 === person2}`)
+// console.log(`JSON.stringify(person1) === JSON.stringify(person2) is ${JSON.stringify(person1) ===
+//         JSON.stringify(person2)}`);
+// console.log(JSON.stringify(person1));
+// console.log(person1.toString());
+// console.log(`name of person1 is ${person1.name}`)
+// console.log(`person1 lives in city ${person1.address.city}`);
+// Object.keys(person1).forEach(k => console.log(k)) //array of the object keys
+// Object.values(person1).forEach(v => console.log(v)); //array of the object values
+// Object.entries(person1).forEach(e => console.log(e)); //array of arrays - [key, value]
+// console.log(Object.entries(person1));
 function createAddress(city, street) {
         //{city: city, street: street} <=> {city, street}
         return {city, street}
@@ -63,4 +63,16 @@ const persons = [
 // 
 /************************************************ */
 
-
+/*************************************************HW 14 solutions */
+//task 3
+function getPersonsCity(persons, city) {
+        return persons.filter(p => p.address.city === city);
+}
+console.log(getPersonsCity(persons, 'Rehovot'));
+//task 4
+function movePersonsNoCityAtBeginning(persons, city) {
+        const personsRes = getPersonsCity(persons, city);
+        personsRes.unshift(...persons.filter(p => p.address.city !== city));
+        return personsRes;
+}
+console.log(movePersonsNoCityAtBeginning(persons,  'Rehovot'));
