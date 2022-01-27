c0e8f474f9f962c2991b4fbeec87d137481d32e4
Yuragranovsky <yuragranovsky@gmail.com>
2022-01-27 16:49:00 +0200

    hw #14

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function myMap(array, callback) {
 // const ar4 = myMap(ar, mapper2)
 // console.log(ar4);
 /***********************************************************cw 14 */
- const ar20 = [13, 17, 20, 23, 2, 40];
+ //const ar20 = [13, 17, 20, 23, 2, 40];
 // const arEvenOdd = ar20.filter((n, _i, a) => a.length % 2 == 0 ?
 //  n % 2 == 0 : n % 2 == 1);
 // console.log(arEvenOdd);
@@ -80,13 +80,13 @@ function myMap(array, callback) {
 //TODO write myFilter(array, callback) based on myForEach
 //callback - function with three possible parameters: current element, current index, reference to array
 /********************************************************************* */
-let res = ar20.reduce((res, cur) => res + cur, 0);
-console.log(res)
-const max = ar20.reduce((max, cur)=>cur > max ? cur : max, ar20[0]);
-console.log(max);
-console.log(res);
-// reduce with no second argument
-res = ar20.reduce((res, cur) => res + cur);
+// let res = ar20.reduce((res, cur) => res + cur, 0);
+// console.log(res)
+// const max = ar20.reduce((max, cur)=>cur > max ? cur : max, ar20[0]);
+// console.log(max);
+// console.log(res);
+// // reduce with no second argument
+// res = ar20.reduce((res, cur) => res + cur);
 /******************************************HW 14 definition task1 */
 //TODO write myReduce(array, callback, initialResult) based on myForEach
 //callback - function with four  possible parameters: accumulated result,
@@ -94,4 +94,41 @@ res = ar20.reduce((res, cur) => res + cur);
 // if the user call doesn't contain initialResult, then the first element of the array will be
 //considered as initial result (in that case iterating begins from the second element of the array)
 /********************************************************************* */
+/**********************************************************HW 14 Solutions */
+function myFilter(array, callbackPredicate) {
+        const res = [];
+        function forEachCallback(n, i, a) {
+                if (callbackPredicate(n, i, a)) {
+                        res.push(n);
+                }
+        }
+        myForEach(array, forEachCallback);
+        return res;
+}
+const ar20 = [13, 17, 20, 23, 2, 40,50];
+const arEvenOdd = myFilter(ar20,(n, _i, a) => a.length % 2 == 0 ?
+ n % 2 == 0 : n % 2 == 1);
+console.log(arEvenOdd);
+function myReduce(array, callbackReduce, initial) {
+        
+        if (initial == undefined) {
+                initial = array[0];
+                array = array.slice(1);
+        }
+        let res = initial;
+        function forEachCallack(n, i, a) {
+                 res = callbackReduce(res,n,i,a );       
+        }
+        myForEach(array,forEachCallack);
+        return res;
+}
+let res = myReduce(ar20, (res, cur) => res + cur, 0);
+console.log(res)
+let max = myReduce(ar20,(max, cur)=>cur > max ? cur : max, ar20[0]);
+console.log(max);
 
+// reduce with no second argument
+res = myReduce(ar20,(res, cur) => res + cur);
+max = myReduce(ar20,(max, cur)=>cur > max ? cur : max);
+console.log(res);
+console.log(max);
