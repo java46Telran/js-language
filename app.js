ab8a341e5d7f55cdc7eda0d23be231426cc78017
Yuragranovsky <yuragranovsky@gmail.com>
2022-01-28 11:49:21 +0200

    cw #15 displayOccurrences & HW #15 definition

--- app.js
+++ app.js
@@ -16,19 +16,19 @@
 // Object.values(person1).forEach(v => console.log(v)); //array of the object values
 // Object.entries(person1).forEach(e => console.log(e)); //array of arrays - [key, value]
 // console.log(Object.entries(person1));
-function createAddress(city, street) {
-        //{city: city, street: street} <=> {city, street}
-        return {city, street}
-}
-function createPerson(id, name, address) {
-        return {id, name, address};
-}
-const persons = [
-        createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
-        createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
-        createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
-        createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
-]
+// function createAddress(city, street) {
+//         //{city: city, street: street} <=> {city, street}
+//         return {city, street}
+// }
+// function createPerson(id, name, address) {
+//         return {id, name, address};
+// }
+// const persons = [
+//         createPerson(123, "Vasya", createAddress("Rehovot","Parshani")),
+//         createPerson(124, "Olya", createAddress("Rehovot","Pr. Plaut")),
+//         createPerson(125, "Tolya", createAddress("Tel-Aviv","Dizengoff")),
+//         createPerson(126, "Sara", createAddress('Lod', 'Sokolov'))
+// ]
 /**************************************HW 14 definition task3 */ 
 
 //TODO write function getPersonsCity(persons, city) , taking array of the persons and a city (see the branch js-objects ) and returning array of the persons living in the given city. For example, find persons living in Rehovot 
@@ -65,14 +65,67 @@ const persons = [
 
 /*************************************************HW 14 solutions */
 //task 3
-function getPersonsCity(persons, city) {
-        return persons.filter(p => p.address.city === city);
-}
-console.log(getPersonsCity(persons, 'Rehovot'));
+// function getPersonsCity(persons, city) {
+//         return persons.filter(p => p.address.city === city);
+// }
+// console.log(getPersonsCity(persons, 'Rehovot'));
 //task 4
-function movePersonsNoCityAtBeginning(persons, city) {
-        const personsRes = getPersonsCity(persons, city);
-        personsRes.unshift(...persons.filter(p => p.address.city !== city));
-        return personsRes;
+// function movePersonsNoCityAtBeginning(persons, city) {
+//         const personsRes = getPersonsCity(persons, city);
+//         personsRes.unshift(...persons.filter(p => p.address.city !== city));
+//         return personsRes;
+// }
+// console.log(movePersonsNoCityAtBeginning(persons,  'Rehovot'));
+/****************************cw #15 */
+//input: ["lmn", "d", "d", "lmn", "a", "lmn", "a", "bc"]
+//output: lmn -> 3
+//        a -> 2
+//        d -> 2
+//        bc -> 1
+function displayOccurrences(array) {
+        //creating object with key - unique element of array (string as an element of array)
+        //                     value - occurrences count
+        //difference between  obj ={a: 123, d: "abc"}; const a = "d" ; obj.a === 123,
+        // and obj[a] === "abc"
+        //obj.c = 10 -> {a: 123, d: "abc", c: 10}
+        const res = {};
+        for (let i = 0; i < array.length; i++) {
+                if (res[array[i]] === undefined) {
+                        //string as content of array[i] occures first time
+                        res[array[i]] = 1;
+                } else {
+                        res[array[i]] = res[array[i]] + 1;
+                }
+        }
+        // console.log(res) -> intermediate debug log
+        Object.entries(res).sort((e1, e2) => {
+                const res = e2[1] - e1[1];
+                return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
+        }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
+
+
 }
-console.log(movePersonsNoCityAtBeginning(persons,  'Rehovot'));
+const ar = ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
+displayOccurrences(ar);
+/**********************************************HW #15 task1 */
+//refactoring of displayOccurrences function from the classwork #15
+//lines 92 - 99 should be a separated function
+//that separated function should apply standard methods like reduce
+
+/**********************************************************HW #15 task2 */
+// write useful function countBy(array, callbackFun) that returns object
+// with keys as grouping criteria  and values as the occurrence counts
+
+//where array - any array, callbackFun - function returning grouping criteria
+//examples:
+// const arr = [6.4, 7.3, 6.5, 6.9];
+//const statistics = countBy(arr, element => Math.floor(element))
+//result: statistics -> {"6": 3, "7":1}
+// const arr = ['abcd', 'lmnr', 'ab', 'dddd'];
+//const statistics = countBy(arr, element => element.length)
+//result: statistics -> {"4": 3, "2":1}
+//const arr = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
+// {age: 25, id: 123, name: 'Vasya'},{age: 70, id: 123, name: 'Vasya'}  ]
+//const statistics = countBy(arr, element -> element.age)
+//result statistics -> {"25":2, "50":1, "70":1}
+
