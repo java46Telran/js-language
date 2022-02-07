cccffaa2ed89b51ef0c4820085e9c852920ed345
Yuragranovsky <yuragranovsky@gmail.com>
2022-02-07 19:48:14 +0200

    HW #17 solution & testOutput for testing all methods

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ class WageEmployee extends Employee {
                 this.#wage = wage;
         }
         computeSalary() {
+                console.log('logging of parameter this',this);
                 return super.computeSalary() + this.#hours * this.#wage
         }
         
@@ -66,7 +67,7 @@ class WageEmployee extends Employee {
 }
 const person4 = new WageEmployee(126, 'Asaf', 1000, 10, 100);
 console.log(`person4 is ${person4}`)
-/**************************************************************HW #17 definition */
+/**************************************************************HW #17 solution */
 const persons = [
         new Child(100, 'Olya', 'Shalom'),
         new Child(101, 'Serega', "Boker"),
@@ -78,14 +79,40 @@ function countOfPersonType(persons, type) {
         //return count of persons of the given type
         //Example:
         //countOfPersonType(persons, 'WageEmployee') ---> 1
+        return getPersonsType(persons, type).length;
 }
+function getPersonsType(persons, type) {
+        return persons.filter(p => p.constructor.name === type);
+}
+
 function computeSalaryBudget(persons) {
         //returns total salary of all employee objects in the given array
         //Example:
         //computeSalaryBudget(persons) ---> 3000
+        const allEmployees = persons.filter(p => !!p.computeSalary);
+        const salaryValues = allEmployees.map(p => p.computeSalary());
+        return salaryValues.reduce((res, cur)=> res + cur)
 }
-function countChildrenGindergarten(persons, kindergarten) {
+function countChildrenKindergarten(persons, kindergarten) {
         //returns number of children in the given kindergarten
         //Example:
-        //countChildrenGindergarten(persons, 'Shalom') ---> 2
+        //countChildrenKindergarten(persons, 'Shalom') ---> 2
+        const allChildren = getPersonsType(persons, "Child");
+        return allChildren.reduce((res, cur) => cur.getKindergarten() === kindergarten ? res + 1 : res, 0)
 }
+const type = "WageEmployee";
+const kindergarten = "Shalom";
+let expected = 1;
+console.log(`function countOfPersonType for type ${type} expected ${expected} actual result is ${countOfPersonType(persons, type)}`);
+expected = 3000;
+console.log(`function computeBudget  expected ${expected} actual result is ${computeSalaryBudget(persons)}`);
+expected = 2;
+console.log(`function countChildrenKindergarten for kindergarten ${kindergarten} expected ${expected} actual result is ${countChildrenKindergarten(persons, kindergarten)}`);
+function testOutput(fun, expected) {
+console.log(`function: ${fun.name} ; expected result: ${expected} ; actual result: ${fun()} `)
+
+}
+//testOutput(WageEmployee.prototype.computeSalary.bind(person4), 2000 )
+//testOutput(computeSalaryBudget.bind(undefined,persons), 3000)
+testOutput(countOfPersonType.bind(undefined,persons,'Child'), 3)
+
